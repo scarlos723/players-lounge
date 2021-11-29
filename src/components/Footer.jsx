518da91210cf9334fbfa998bfeb0c1106652a63d
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-11-29 18:07:55 -0500

    assets folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="navbar">
-      <h1>Footer baby</h1>
-    </div>
+    <section className='Footer'>
+
+      <section className='Footer-cards'>
+        <h1>CHECK OUT COLLECTIBLES</h1>
+        <img className='' alt='Carles Puyol'/>
+        <img className='' alt='Didier Drogba'/>
+        <img className='' alt='Jhon Terry'/>
+        <img className='' alt='Roberto Carlos'/>
+      </section>
+
+      <section className='Footer-links'>
+        <h3>Links</h3>
+      </section>
+
+    </section>
   );
 }
 
