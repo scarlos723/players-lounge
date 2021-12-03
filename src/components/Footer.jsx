cff7c29a1877c2ff75cb4661f76bd15278aa5f4b
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-12-03 10:11:13 -0500

    Desktop footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/components/Footer.css'
 import arrow from '../assets/footer/arrow.png'
 import logo from '../assets/footer/playerlounge.png'
+import sm from '../assets/footer/socialmedia.png'
 
 const Footer = () => {
   return (
@@ -19,8 +20,14 @@ const Footer = () => {
       </section>
 
       <section className='Footer-links'>
-        <img src={logo} alt='Player lounge' />
-        <h3>Links</h3>
+        <div className='Footer-links-left'>
+          <img src={logo} alt='Player lounge' />
+          <a href='#'>Players</a>
+          <a href='#'>Marketplace</a>
+          <a href='#'>How it works</a>
+          <a href='#'>Contact us</a>
+          <img src={sm} alt='social media' />
+        </div>
         <p>Copyright, 2021 Player Lounge</p>
       </section>
 
